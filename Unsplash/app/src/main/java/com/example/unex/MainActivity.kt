da18653501600ca70431a512787c351966b5f8b3
HamBeomJoon <hbj0802@naver.com>
2024-06-08 11:47:27 +0900

    unsplash Example

---
+++ Unsplash/app/src/main/java/com/example/unex/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.unex
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.unex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
