e87a3eae582546ea9fd53fe018a0291f65267cb0
HamBeomJoon <hbj0802@naver.com>
2024-06-26 01:09:35 +0900

    프로젝트 setting

---
+++ 1주차/app/src/main/java/com/example/app/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
